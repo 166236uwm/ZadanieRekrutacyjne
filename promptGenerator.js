c490be22f0e682f991bd542f98e7d62eaa62c28a
166236uwm <92304880+166236uwm@users.noreply.github.com>
2024-11-15 15:38:10 +0100

    final commit
    Skończono zadanie dodatkowe, zmodyfikowano prompt tak aby LLM zwracał bardziej jednostajne wyniki

--- promptGenerator.js
+++ promptGenerator.js
@@ -1,14 +1,23 @@
 function createUserPrompt(articleContent) {
     return `
-      Przetwórz poniższą treść na HTML, używając odpowiednich tagów nagłówków, akapitów i wypunktowania. Nie dodawaj komentarzy, stylów CSS ani skryptów JavaScript.
-      Nie zmieniaj kolejności oryginalnych elementów, ani ich treści.
-      Zwrócony kod powinien zawierać wyłącznie zawartość do wstawienia w tag <body> (bez tagu body lub komentarzy takich jak - \`\`\`html\`\`\`).
-      Tam gdzie uważasz że pasowałby obrazek dodaj tag <img> z atrybutem src="image_placeholder.jpg". 
+      Poniżej jest artykuł, przetwórz go odpowiednio, aby można było wkleić go w <body> strony internetowej. Nie usuwaj oryginalnych elementów ani ich treści.
+      Tam gdzie uważasz że pasowałby obrazek dodaj tag <img> z atrybutem src="image_placeholder.jpg", umieść obrazek i opis obrazka w <figure>.
       Dodaj atrybut alt z dokładnym promptem który mógłby zostać użyty do wygenerowania obrazka. 
       Prompt do generowania obrazka powinien spełniać wymogi: 
       opisowy, precyzujący kluczowe elementy obrazka oraz jego nastrój.
       Umieść krótkie podpisy (max 4 słowa) pod obrazkami używając tagu <figcaption>.
-  
+      Tak przygotowany <figure> umieść w jednym <div> wraz z <p> do którego odnosi się obrazek.  
+      Artykuł powinien mieć tyle obrazków ile jest paragrafów w oryginalnym tekście oraz tyle divów ile jest obrazków.
+      Jeden akapit powinien wyglądać tak: 
+      <h1>tytuł akapitu</h1>
+      <div>
+        <p>teść</p>
+        <figure>
+          <img>
+          <figcaption></figcaption>
+        </figure>
+      </div>
+      Jeżeli nie sprecyzowałem co zrobić z niespodziewanym elementem, zdecyduj samodzielnie co z nim zrobić ale pod żadnym pozorem nie usuwaj go.
       Treść artykułu:
       ${articleContent}
     `;
@@ -16,7 +25,8 @@ function createUserPrompt(articleContent) {
   function createSystemPrompt() {
     return `
       Jesteś genialnym programistą piszącym tylko w HTML. 
-      Niestety nie dodajesz komentarzy, stylów CSS ani skryptów JavaScript oraz robisz tylko i dokładnie to o co zostaniesz poproszony.
+      Nie dodawaj komentarzy, stylów CSS ani skryptów JavaScript. Nie zmieniaj kolejności oryginalnych elementów, ani ich treści.
+      Zwrócony kod powinien zawierać wyłącznie zawartość do wstawienia w tag <body> (bez tagu body lub komentarzy takich jak - \`\`\`html\`\`\`).
     `;
   }
   
